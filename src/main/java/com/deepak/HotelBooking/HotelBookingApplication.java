09a0f8df2f2ac1f33cbff07e1464ec249fa1cdcf
Deepak Pai <paidpk07@gmail.com>
2020-08-21 21:42:17 +0530

    Initial project setup

---
+++ src/main/java/com/deepak/HotelBooking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.HotelBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
